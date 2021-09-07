0f093c8d25d68197d1c1ad6c5aaf356da1b803c7
sang-genetica <85684528+sang-genetica@users.noreply.github.com>
2021-09-07 18:11:11 +0700

    Test Commit

---
+++ Test Suites/End to End.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>End to End</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>91486305-f768-4bf2-98e5-3522dde1d0f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>08d0da97-7f20-4fe8-a5c5-4ed200ae6956</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8e860f2-d51b-4017-9bce-634634a285d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
